7d45b65f05987c33de6732753fab911a3b67dfbf
Felipej3ds <felipej.d.silvadf@gmail.com>
2024-07-30 19:36:01 -0300

    acrescentando o conteudo de operadores

---
+++ 4_operadores_aritimericos.py
@@ -0,0 +1,41 @@
+#Operadores aritiméticos, Expressoes e atribuições aritiméticas
+
+#soma +
+a = 2 + 2
+print (a)
+
+#subtração -
+a = 2 - 2
+print(a)
+
+#Multiplicação
+a = 4 * 4
+print(a)
+
+#Divisão
+a = 5 / 5
+print(a)
+
+#precedência
+"""  
+as operações são atribuidas na seguinte ordem
+*
+/
++
+- 
+O que está entre parenteses possui precendencia
+""" 
+
+
+
+print(.356%2) # .(numeros_Decimais)%
+print(3 % 2 == 0 )  
+
+resto = int(5 % 2)
+print(resto)
+
+nomeri1 = float(input("digite um numero: "))
+nomeri2 = float(input("digite um numero: "))
+nomeri3 = float(input("digite um numero: "))
+
+print(nomeri1%3)
