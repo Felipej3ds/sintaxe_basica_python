82bf26d6abef71e6e74c22264eca4eee78ffd7fc
Felipej3ds <felipej.d.silvadf@gmail.com>
2024-07-30 19:26:50 -0300

    Atualizando o rquivo de variaveis

--- 1_variaveis.py
+++ 1_variaveis.py
@@ -7,11 +7,23 @@
     - atribuição(conteudo)
 """
 
-"tipos de dados"
+#Regras de nomeação_de_variaveis:
+"""
+    o python é uma linguagem case sensitive 
+    variaveis não podem ser numeros
+    não podem possuir caracteres especiais
+    um código não pode ter duas variaveis iguais
+"""
+
+"tipos de dados Primitivos"
 
 #int (inteiro)
-#str (tipo texto)
+idade = 20
+#str (tipo texto) deve
+nome = 'Fulano'
 #float (ponto flutuante)
+altura = 1.8
 #bool: booleano ou lógico (true/false)
+fulano_e_menor_de_idade = False
 
 "so sera convertido se for possivel converter"
